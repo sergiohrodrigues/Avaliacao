5de3c4c95f7edf0c54f7f982120da5485fea7381
Sergio Henrique Rodrigues <86135798+sergiohrodrigues@users.noreply.github.com>
2022-11-09 18:55:44 -0300

    estilizando botoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,43 +5,38 @@ let four = document.querySelector('#four')
 let five = document.querySelector('#five')
 
 one.addEventListener('click', () => {
-    let um = one.style.backgroundColor = '#7C8798';
+    one.style.backgroundColor = '#7C8798';
     one.addEventListener('click', () => {
         one.style.backgroundColor = '#262E38';
     })
-    one.style.backgroundColor.hover = '#FC7614'
 })
 
 two.addEventListener('click', () => {
-    let dois = two.style.backgroundColor = '#7C8798';
+    two.style.backgroundColor = '#7C8798';
     two.addEventListener('click', () => {
         two.style.backgroundColor = '#262E38';
     })
-    two.style.backgroundColor.hover = '#FC7614'
 })
 
 three.addEventListener('click', () => {
-    let tres = three.style.backgroundColor = '#7C8798';
+    three.style.backgroundColor = '#7C8798';
     three.addEventListener('click', () => {
         three.style.backgroundColor = '#262E38';
     })
-    three.style.backgroundColor.hover = '#FC7614'
 })
 
 four.addEventListener('click', () => {
-    let quatro = four.style.backgroundColor = '#7C8798';
+    four.style.backgroundColor = '#7C8798';
     four.addEventListener('click', () => {
         four.style.backgroundColor = '#262E38';
     })
-    four.style.backgroundColor.hover = '#FC7614'
 })
 
 five.addEventListener('click', () => {
-    let cinco = five.style.backgroundColor = '#7C8798';
+    five.style.backgroundColor = '#7C8798';
     five.addEventListener('click', () => {
         five.style.backgroundColor = '#262E38';
     })
-    five.style.backgroundColor.hover = '#FC7614'
 })
 
 
